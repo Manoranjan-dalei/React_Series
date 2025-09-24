73cb76519931f0ba0dbb2b964725ad64f5b09c73
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-24 12:20:09 +0530

    commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+  <h1>Hello Happy</h1>
+  );
+}
+
+export default App;
---
+++ frontend/01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Hello Happy react with vite </h1>
+  )
+}
+
+export default App
